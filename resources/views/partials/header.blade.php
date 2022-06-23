1aa033b7a2400f71a82e4506d633d3560cc3a95c
Luca Gaddini <90861515+lucagad@users.noreply.github.com>
2022-06-23 14:53:00 +0200

    {Laravel} Added: Header & Footer partial views

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,18 @@
+<header class="container-lg">
+
+    <div class="logo">
+      <a href="#"><img src="{{asset('img/dc-logo.png')}}" alt="DC Logo"></a>
+    </div>
+
+    <nav>
+      <ul>
+        <li><a href="#">Characters</a></li>
+        <li><a href="#">Characters</a></li>
+        <li><a href="#">Characters</a></li>
+        <li><a href="#">Characters</a></li>
+        <li><a href="#">Characters</a></li>
+        <li><a href="#">Characters</a></li>
+      </ul>
+    </nav>
+
+  </header>
